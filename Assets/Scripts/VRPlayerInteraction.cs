b3e18b90e4646832b47e836532d141c9f7c1a165
Zinzed <zineb.amezyane0@gmail.com>
2026-03-12 15:40:21 -0400

    crosshair works now for VRPlayer
    - Attached vrPlayerInteraction script to player

--- Assets/Scripts/VRPlayerInteraction.cs
+++ Assets/Scripts/VRPlayerInteraction.cs
@@ -1,55 +1,25 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.XR.Interaction.Toolkit;
 
 public class VRPlayerInteraction : MonoBehaviour
 {
     public float interactRange = 10.0f;
-    public Transform playerCamera;
+    public Transform playerCamera; // VR Camera (usually XR Origin/Camera Offset/Main Camera)
     public uiBehaviour uiBehaviourScript;
     public wandBehaviour wandBehaviourScript;
 
-    //vr controls references
-    public InputActionReference grabAction;
-
     public bool canInteract;
     public int enemiesKilled;
-    public bool isGrabbing;
 
     private enemyBehaviour enemy;
-    private bool grabPressedThisFrame;
-    private bool wasGrabPressed;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         canInteract = false;
-        isGrabbing = false;
-
-        // Enable the input action
-        if (grabAction != null)
-            grabAction.action.Enable();
     }
 
-    void OnEnable()
-    {
-        if (grabAction != null)
-            grabAction.action.Enable();
-    }
-
-    void OnDisable()
-    {
-        if (grabAction != null)
-            grabAction.action.Disable();
-    }
-
-
-    // Update is called once per frame
     void Update()
     {
-        // Handle toggle grab input
-        HandleGrabToggle();
-
         canInteract = false;
         enemy = null;
         Color currentTargetColor = Color.clear;
@@ -90,61 +60,7 @@ public class VRPlayerInteraction : MonoBehaviour
         }
 
         uiBehaviourScript.SetInteract(canInteract);
-
-        // Check if we're grabbing AND have an enemy targeted
-        if (enemy != null && isGrabbing)
-        {
-            // Get the wand light color from your wand script
-            Color wandColor = wandBehaviourScript.CurrentColor;
-
-            // Calculate "distance" between colors to handle slight rounding differences
-            float colorDiff = Vector4.Distance(currentTargetColor, wandColor);
-
-            if (colorDiff < 0.1f) // 0.1 allows for tiny variations
-            {
-                Destroy(enemy.gameObject, 1.2f);
-                enemiesKilled++;
-                Debug.Log("Color Match! Enemy Destroyed.");
-
-                
-            }
-            else
-            {
-                Debug.Log("Wrong Color! Wand: " + wandColor + " vs Enemy: " + currentTargetColor);
-            }
-        }
-    }
-
-    void HandleGrabToggle()
-    {
-        if (grabAction == null) return;
-
-        // Check if grab button was pressed this frame
-        grabPressedThisFrame = grabAction.action.WasPressedThisFrame();
-
-        // Toggle grab state on button press (not hold)
-        if (grabPressedThisFrame && !wasGrabPressed)
-        {
-            isGrabbing = !isGrabbing;
-            Debug.Log("Grab toggled: " + isGrabbing);
-
-           
-        }
-
-        wasGrabPressed = grabPressedThisFrame;
     }
 
     
-
-    // Optional: Public method to manually set grab state if needed
-    public void SetGrabState(bool grabState)
-    {
-        isGrabbing = grabState;
-    }
-
-    // Optional: Toggle grab state manually
-    public void ToggleGrab()
-    {
-        isGrabbing = !isGrabbing;
-    }
 }
