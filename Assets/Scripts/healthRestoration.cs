b36838139b8f57a9d9285ae73d04e71f6cae881b
Naomi <leronnaomi@gmail.com>
2026-03-28 18:58:20 -0400

    Health restoration chamber visuals
    Implemented health restoration chamber visually

---
+++ Assets/Scripts/healthRestoration.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class healthRestoration : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private string trigger = "isInside";
+
+    [SerializeField] private AudioSource spin;
+
+    private void OnTriggerEnter(Collider other)
+    {
+       if(other.CompareTag("Player"))
+        {
+            animator.SetTrigger(trigger);
+            spin.Play();
+        }
+    }
+    //// Start is called once before the first execution of Update after the MonoBehaviour is created
+    //void Start()
+    //{
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+}
