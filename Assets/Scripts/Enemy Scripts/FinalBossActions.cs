31460f904441e2f0b44a66284972ae230152f18a
Naomi <leronnaomi@gmail.com>
2026-04-03 00:01:04 -0400

    Attack enhancements
    Attacks are more random and feel more robust
    Still need to implement damage

--- Assets/Scripts/Enemy Scripts/FinalBossActions.cs
+++ Assets/Scripts/Enemy Scripts/FinalBossActions.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FinalBossActions : MonoBehaviour
@@ -14,6 +15,15 @@ public class FinalBossActions : MonoBehaviour
     private bool isDead = false;
     private bool isAttacking = false;
 
+    [SerializeField] private int minAttacks = 2;
+    [SerializeField] private int maxAttacks = 5;
+    [SerializeField] private float timeBetweenAttacks = 1.5f;
+    private float attackDuration = 1.06f;
+
+    [SerializeField] private float teleportCooldown = 10f;
+    [SerializeField] private float safeTimeBeforeTeleport = 2.0f;
+    private float lastTeleportTime;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -27,6 +37,8 @@ public class FinalBossActions : MonoBehaviour
             return;
         }
 
+        lastTeleportTime = Time.time;
+
         if (teleportEffect != null)
         {
             //teleportEffect.transform.position = transform.position;
@@ -39,12 +51,9 @@ public class FinalBossActions : MonoBehaviour
 
             if (teleportCount >= teleportsBeforeAttack)
             {
-                if (!isAttacking)
-                {
                     teleportCount = 0;
                     TriggerAttack();
-                    SetNextAttack();
-                }
+                    SetNextAttack();                
             }
         }   
     }
@@ -56,27 +65,42 @@ public class FinalBossActions : MonoBehaviour
             return; 
         }
 
-        isAttacking = true;
-        animator.SetTrigger("isAttacking");
+        StartCoroutine(AttackSequence());       
+    }
 
-        StartCoroutine(AttackFxRoutine());
-        StartCoroutine(ResetAttack());
+    IEnumerator AttackSequence()
+    {
+        isAttacking = true;
+
+        int attackCount = Random.Range(minAttacks, maxAttacks);
+
+        
+
+        for (int i = 0; i < attackCount; i++)
+        {
+            if (Time.time - lastTeleportTime > teleportCooldown - safeTimeBeforeTeleport)
+            {
+                break;
+            }
+
+            animator.ResetTrigger("isAttacking");
+            animator.SetTrigger("isAttacking");
+            StartCoroutine(AttackFxRoutine());
+
+            yield return new WaitForSeconds(attackDuration + timeBetweenAttacks);
+        }
+
+        isAttacking = false;
     }
 
     IEnumerator AttackFxRoutine()
     {
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(0.2f);
 
         if (attackEffect != null)
         {
             attackEffect.Play();
         }
-    }
-
-    IEnumerator ResetAttack()
-    {
-        yield return new WaitForSeconds(1.06f);
-        isAttacking = false;
     }
 
     public void Die()
